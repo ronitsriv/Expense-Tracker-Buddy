dc139239132edc469a132a3b9d56e8a238d0b26e
sahim <sahim.mohammad@gmail.com>
2024-04-03 13:46:53 +0530

    Sahim-3503

--- src/main/java/com/example/ExpenseManager/demo/DemoApplication.java
+++ src/main/java/com/example/ExpenseManager/demo/DemoApplication.java
@@ -2,12 +2,12 @@ package com.example.ExpenseManager.demo;
 //header
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//sahim
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+//sahim here
 }

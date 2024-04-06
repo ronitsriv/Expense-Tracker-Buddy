ca31cf2589ac0ab54c6e1f74d9a4dbc4930ce937
saroshcruz <147895916+saroshcruz@users.noreply.github.com>
2024-04-06 23:09:38 +0530

    Update CategoryWIseBudgetService.java

--- src/main/java/com/example/ExpenseManager/demo/categoryWiseBudgetEntry/CategoryWIseBudgetService.java
+++ src/main/java/com/example/ExpenseManager/demo/categoryWiseBudgetEntry/CategoryWIseBudgetService.java
@@ -1,6 +1,5 @@
 package com.example.ExpenseManager.demo.categoryWiseBudgetEntry;
 
-import com.example.ExpenseManager.demo.categoryEntry.Category;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;

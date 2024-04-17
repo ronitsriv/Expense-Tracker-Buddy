125d2a199b875eada859a4a7138dd380c16d0047
Sahim-3503 <142649952+Sahim-3503@users.noreply.github.com>
2024-04-17 23:32:32 +0530

    Update categoryController.java

--- src/main/java/com/example/ExpenseManager/demo/categoryEntry/categoryController.java
+++ src/main/java/com/example/ExpenseManager/demo/categoryEntry/categoryController.java
@@ -2,23 +2,38 @@ package com.example.ExpenseManager.demo.categoryEntry;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+
 @Controller
-public class CategoryController {
+public class categoryController {
 
     private final CategoryService categoryService;
+    private final CategoryRepositoryQueries categoryRepositoryQueries;
 
-    public CategoryController(CategoryService categoryService) {
+    public categoryController(CategoryService categoryService, CategoryRepositoryQueries categoryRepositoryQueries) {
         this.categoryService = categoryService;
+        this.categoryRepositoryQueries = categoryRepositoryQueries;
     }
 
-    @RequestMapping("categories")
-    public String listAllCategories(ModelMap model) {
-        List<Category> categories = categoryService.getAllCategories();
-        model.addAttribute("categories", categories);
-        return "listCategories"; // Assuming you have a corresponding view named "listCategories"
-    }
+    // @RequestMapping("categories")
+    // public String listAllCategories(ModelMap model) {
+    //     List<Category> categories = categoryService.getAllCategories();
+    //     model.addAttribute("categories", categories);
+    //     return "listCategories"; // Assuming you have a corresponding view named "listCategories"
+    // }
+
+ // Mapping to display all reminders
+ @GetMapping("categories")
+ public String listAllCategories(ModelMap model) {
+     String username = "John Doe";
+     List<Category> categories = categoryRepositoryQueries.findByUsername(username);
+     model.addAttribute("category", categories);
+     return "listCategories";
+ }
+
+
 }

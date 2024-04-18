bfeb55c8d56db97c64dddb5f64a5e524a2993901
Sahim-3503 <142649952+Sahim-3503@users.noreply.github.com>
2024-04-18 22:47:41 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,15 +5,15 @@ INSERT INTO category (category_id, category_name, username) VALUES (2, 'Utilitie
 -- Insert data into the reminder table with custom IDs
 INSERT INTO reminder (amount, category_id, done, reminder_date, reason, username) VALUES (50, 1, false, CURRENT_DATE(), 'Buy groceries', 'John Doe');
 INSERT INTO reminder (amount, category_id, done, reminder_date, reason, username) VALUES (100, 1, false, CURRENT_DATE(), 'Pay electricity bill', 'John Doe');
-INSERT INTO reminder (amount, category_id, done, reminder_date, reason, username) VALUES (150, 2, false, CURRENT_DATE(), 'Pay A bill', 'John Doe');
-INSERT INTO reminder (amount, category_id, done, reminder_date, reason, username) VALUES (25, 2, false, CURRENT_DATE(), 'Pay B bill', 'John Doe');
+INSERT INTO reminder (amount, category_id, done, reminder_date, reason, username) VALUES (150, 1, false, CURRENT_DATE(), 'Pay A bill', 'John Doe');
+INSERT INTO reminder (amount, category_id, done, reminder_date, reason, username) VALUES (25, 1, false, CURRENT_DATE(), 'Pay B bill', 'John Doe');
 
 -- Insert data into the amount table
 INSERT INTO amount (amount_id, amount, entry_date, reason, username, category_id) VALUES (1, 50, CURRENT_DATE(), 'Groceries', 'John Doe', 1);
 INSERT INTO amount (amount_id, amount, entry_date, reason, username, category_id) VALUES (2, 100, CURRENT_DATE(), 'Electricity Bill', 'John Doe', 1);
-INSERT INTO amount (amount_id, amount, entry_date, reason, username, category_id) VALUES (3, 250, CURRENT_DATE(), 'A Bill', 'John Doe', 2);
-INSERT INTO amount (amount_id, amount, entry_date, reason, username, category_id) VALUES (4, 25, CURRENT_DATE(), 'B Bill', 'John Doe', 2);
+INSERT INTO amount (amount_id, amount, entry_date, reason, username, category_id) VALUES (3, 150, CURRENT_DATE(), 'A Bill', 'John Doe', 1);
+INSERT INTO amount (amount_id, amount, entry_date, reason, username, category_id) VALUES (4, 25, CURRENT_DATE(), 'B Bill', 'John Doe', 1);
 
 -- Insert data into the category_wise_budget table
 INSERT INTO category_wise_budget (cat_wiseid, budget, username, category_id) VALUES (1, 500, 'John Doe', 1);
-INSERT INTO category_wise_budget (cat_wiseid, budget, username, category_id) VALUES (2, 300, 'John Doe', 2);
+-- INSERT INTO category_wise_budget (cat_wiseid, budget, username, category_id) VALUES (1, 300, 'John Doe', 2);

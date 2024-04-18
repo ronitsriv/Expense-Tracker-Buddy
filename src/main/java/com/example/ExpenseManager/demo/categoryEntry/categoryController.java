d9defda461cea356c3e4f511728a254314729493
Sahim-3503 <142649952+Sahim-3503@users.noreply.github.com>
2024-04-18 11:55:11 +0530

    Update categoryController.java

--- src/main/java/com/example/ExpenseManager/demo/categoryEntry/categoryController.java
+++ src/main/java/com/example/ExpenseManager/demo/categoryEntry/categoryController.java
@@ -1,9 +1,14 @@
 package com.example.ExpenseManager.demo.categoryEntry;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jakarta.validation.Valid;
 
 import java.util.List;
 
@@ -14,6 +19,7 @@ public class categoryController {
     private final CategoryService categoryService;
     private final CategoryRepositoryQueries categoryRepositoryQueries;
 
+    @Autowired
     public categoryController(CategoryService categoryService, CategoryRepositoryQueries categoryRepositoryQueries) {
         this.categoryService = categoryService;
         this.categoryRepositoryQueries = categoryRepositoryQueries;
@@ -35,5 +41,21 @@ public class categoryController {
      return "listCategories";
  }
 
+
+ @RequestMapping(value="add-category", method = RequestMethod.POST)
+ public String addNewCategory(ModelMap model, @Valid Category category, BindingResult result) {
+     if (result.hasErrors()) {
+         return "newCategory";
+     }
+     categoryService.addCategory(category.getCategoryName(), category.getUsername());
+     return "redirect:categories";
+ }
+ 
+
+ @RequestMapping(value = "add-category", method = RequestMethod.GET)
+ public String showNewCategoryForm(ModelMap model) {
+     model.addAttribute("category", new Category()); // Create a new category object and add it to the model
+     return "newCategory";
+ }
 
 }

edc1258328d89890e1384cf99ae1d30dc70ed385
saroshcruz <147895916+saroshcruz@users.noreply.github.com>
2024-04-23 10:24:48 +0530

    Update CategoryWiseBudget.java

--- src/main/java/com/example/ExpenseManager/demo/categoryWiseBudgetEntry/CategoryWiseBudget.java
+++ src/main/java/com/example/ExpenseManager/demo/categoryWiseBudgetEntry/CategoryWiseBudget.java
@@ -1,17 +1,19 @@
 package com.example.ExpenseManager.demo.categoryWiseBudgetEntry;
 
 import com.example.ExpenseManager.demo.categoryEntry.Category;
+
 import jakarta.persistence.*;
 
 @Entity
 public class CategoryWiseBudget {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cat_wiseid") // Corrected column name
     private int catWiseid;
 
     private int budget;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "categoryId")
     private Category category;
 
@@ -20,8 +22,7 @@ public class CategoryWiseBudget {
     public CategoryWiseBudget() {
     }
 
-    public CategoryWiseBudget(int catWiseid, int budget,String username, Category category) {
-        this.catWiseid=catWiseid;
+    public CategoryWiseBudget(int budget, String username, Category category) {
         this.budget = budget;
         this.category = category;
         this.username = username;
